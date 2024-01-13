cf5abbdd1799fc2051b1e62601f4fffd377b1a99
Jordan Ballard <95947696+jordanmichb@users.noreply.github.com>
2024-01-12 16:27:02 -0800

    Write functions for getting player input
    getPlayerChoice() and getNumberOfGames() both get player input and
    validates it.

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+const choices = ["Rock", "Paper", "Scissors"];
+
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
     const computerChoice = Math.floor(Math.random() * 3); // Randomly choose either 0, 1, or 2
     return choices[computerChoice]; // Return random choice from the list
 }
 
+function getPlayerChoice() {
+    const choice = prompt("Choose rock, paper, or scissors.");
+    choice = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
+    return choices.includes(choice) ? choice
+                                    : "Invalid input."
+}
+
+function getNumberOfGames() {
+    const num = Number(prompt("How many games would you like to play? Enter a positive number."));
+    return isNaN(num) || num < 1 ? "Invalid number." 
+                                 : num;
+}
+
 function playRound(playerSelection, computerSelection) {
     // Format playerSelection so case doesn't matter
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     const tie = "Tie!";
     const win = `You win! Your ${playerSelection} beats the computer's ${computerSelection}.`;
     const lose = `You lose! The computer's ${computerSelection} beats your ${playerSelection}.`;
@@ -28,6 +41,18 @@ function playRound(playerSelection, computerSelection) {
             else if (computerSelection == "Paper") { return win}
             else if (computerSelection == "Scissors") { return tie };
         default:
-            return "Invalid input.";
+            return;
     }
 }
+
+/*
+function game() {
+    const num = getNumberOfGames();
+    
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+}
+
+game();
+*/
