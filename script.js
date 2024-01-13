687021583b6408470181d95abcde6b5e8dd0115b
Jordan Ballard <95947696+jordanmichb@users.noreply.github.com>
2024-01-12 17:44:14 -0800

    Finished game() function
    Made game playable, rearrange some code such as validation, messages,
    cancelled prompts.

--- script.js
+++ script.js
@@ -6,53 +6,99 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-    const choice = prompt("Choose rock, paper, or scissors.");
-    choice = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
-    return choices.includes(choice) ? choice
-                                    : "Invalid input."
+    let choice = prompt("Choose rock, paper, or scissors.");
+    
+    if (choice != null) {
+        // Format player choice so case doesn't matter
+        choice = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase()
+        while (!choices.includes(choice)) {
+            choice = prompt("Invalid choice, please try again.");
+            if (choice == null) { return choice };
+        }
+    }
+
+    return choice;
 }
 
 function getNumberOfGames() {
-    const num = Number(prompt("How many games would you like to play? Enter a positive number."));
-    return isNaN(num) || num < 1 ? "Invalid number." 
-                                 : num;
+    let num = Number(prompt("How many games would you like to play? Enter a positive number."));
+
+    // If number is invalid, continue to promp
+    while (isNaN(num) || num < 0) {
+        num = Number(prompt("Invalid number, please try again."));
+        if (num == 0) { return num }
+    }
+
+    return num;
 }
 
-function playRound(playerSelection, computerSelection) {
-    // Format playerSelection so case doesn't matter
-    const tie = "Tie!";
-    const win = `You win! Your ${playerSelection} beats the computer's ${computerSelection}.`;
-    const lose = `You lose! The computer's ${computerSelection} beats your ${playerSelection}.`;
+function playRound(playerChoice, computerChoice) {
+    const tie = "tie";
+    const win = "win";
+    const lose = "lose";
 
-    console.log(`You chose ${playerSelection}.`);
-    console.log(`The computer chose ${computerSelection}.`);
+    console.log(`You chose ${playerChoice}.`);
+    console.log(`The computer chose ${computerChoice}.`);
 
-    switch (playerSelection) {
+    switch (playerChoice) {
         case "Rock":
-            if (computerSelection == "Rock") { return tie }
-            else if (computerSelection == "Paper") { return lose}
-            else if (computerSelection == "Scissors") { return win };
+            if (computerChoice == "Rock") { return tie }
+            else if (computerChoice == "Paper") { return lose}
+            else if (computerChoice == "Scissors") { return win };
         case "Paper":
-            if (computerSelection == "Rock") { return win }
-            else if (computerSelection == "Paper") { return tie}
-            else if (computerSelection == "Scissors") { return lose };
+            if (computerChoice == "Rock") { return win }
+            else if (computerChoice == "Paper") { return tie}
+            else if (computerChoice == "Scissors") { return lose };
         case "Scissors":
-            if (computerSelection == "Rock") { return lose }
-            else if (computerSelection == "Paper") { return win}
-            else if (computerSelection == "Scissors") { return tie };
+            if (computerChoice == "Rock") { return lose }
+            else if (computerChoice == "Paper") { return win}
+            else if (computerChoice == "Scissors") { return tie };
         default:
             return;
     }
 }
 
-/*
 function game() {
-    const num = getNumberOfGames();
+    const numGames = getNumberOfGames();
+    // Stop game if player chose "Cancel"
+    if (numGames === null || numGames === 0) { return };
     
-    for (let i = 0; i < 5; i++) {
-        playRound();
+    let i, playerWins = 0, computerWins = 0, tieGames = 0;
+
+    for (i = 0; i < numGames; i++) {
+        const playerChoice = getPlayerChoice();
+        // Stop game if player chose "Cancel"
+        if (playerChoice === null) { break };
+        const computerChoice = getComputerChoice();
+        const roundResult = playRound(playerChoice, computerChoice);
+
+        switch (roundResult) {
+            case "win":
+                console.log(`Round won! Your ${playerChoice} beats the computer's ${computerChoice}.`);
+                console.log("");
+                playerWins++;
+                break;
+            case "lose":
+                console.log(`Round lost! The computer's ${computerChoice} beats your ${playerChoice}.`);
+                console.log("");
+                computerWins++;
+                break;
+            case "tie":
+                console.log("Round tied!");
+                console.log("");
+                tieGames++;
+                break;
+            default:
+                break;
+        }
     }
+
+    console.log(`Games played: ${i}`);
+    console.log(`Score:\n\tPlayer: ${playerWins}\n\tComputer: ${computerWins}\n\tTies: ${tieGames}`);
+
+    if (playerWins > computerWins) { console.log("Congratulations! You win the game.") }
+    else if (playerWins < computerWins) { console.log("Sorry! You lost the game.") }
+    else { console.log("Tie game!") };
 }
 
 game();
-*/
